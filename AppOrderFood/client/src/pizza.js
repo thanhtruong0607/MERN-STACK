54736598d2bf875bb71654e0b1a490fb9e0a9271
Nguyen Thanh Truong <truong111120@gmail.com>
2022-03-20 17:17:53 +0700

    commit

---
+++ AppOrderFood/client/src/pizza.js
@@ -0,0 +1,57 @@
+const pizzas =[
+	{
+		name: "PEPPER BARBECUE CHICKEN",
+		varients: [
+			"small",
+			"medium",
+			"large"
+		],
+		prices: [
+			{
+				small: 200,
+				medium: 350,
+				large: 400
+			}
+		],
+		category: "nonveg",
+		image: "https://5.imimg.com/data5/LY/TG/MY-29540739/pepper-barbecue-and-onion-500x500.png",
+		description: "Pepper Barbecue Chicken I Cheese"
+	},
+	{
+		name: "NON VEG SUPREME",
+		varients: [
+			"small",
+			"medium",
+			"large"
+		],
+		prices: [
+			{
+				small: 200,
+				medium: 350,
+				large: 400
+			}
+		],
+		category: "nonveg",
+		image: "https://us2guntur.com/images//10024img/NVSuperme_B_260216.jpg",
+		description: "Pepper Barbecue Chicken I Cheese"
+	},
+	{
+		name: "Golden Corn Pizza",
+		varients: [
+			"small",
+			"medium",
+			"large"
+		],
+		prices: [
+			{
+				small: 180,
+				medium: 250,
+				large: 320
+			}
+		],
+		category: "nonveg",
+		image: "https://3.imimg.com/data3/JY/PG/MY-18318452/images-menu-138-500x500.jpg",
+		description: "Pepper Barbecue Chicken I Cheese"
+	},
+];
+export default pizzas
